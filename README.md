642e53b10ac35d6b3a54c73b346c872cc35165e9
Jono Hill <jono@hillnz.com>
2025-01-27 11:01:48 +1300

    v1

---
+++ README.md
@@ -0,0 +1,6 @@
+# woolworths-tweaks
+
+A userscript to make woolworths.co.nz a bit better.
+
+Current features
+- Replace bonus Everyday Rewards points with the dollar value of the points
---
+++ woolworths-tweaks.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name        Woolworths Tweaks
+// @description Tweaks for woolworths.co.nz
+// @match       *://*.woolworths.co.nz/*
+// ==/UserScript==
+
+// @ts-check
+
+/** 
+ * Replace everyday rewards points with dollar value 
+ * @param {Node} node
+ */
+function monetiseEdrPoints(node) {
+    const CONVERSION_RATE = 15 / 2000;
+    const RE_POINTS = /(\d+) points/;
+    const SELECTORS = [
+        '.isBoostOffer .productStrap-text',
+        '.boost-strap p',
+        '.boost-details-card__body__content__header',
+    ];
+
+    if (node.nodeType !== Node.ELEMENT_NODE) {
+        return;
+    }
+
+    const element = /** @type {Element} */ (node);
+
+    // Try child elements
+    for (const selector of SELECTORS) {
+        const matchingDescendants = element.querySelectorAll(selector);
+        matchingDescendants.forEach(monetiseEdrPoints);
+    }
+
+    if (!element.textContent) {
+        return;
+    }
+
+    if (!SELECTORS.some((selector) => element.matches(selector))) {
+        return;
+    }
+
+    const match = RE_POINTS.exec(element.textContent);
+    if (match) {
+        const points = parseInt(match[1]);
+        const value = points * CONVERSION_RATE;
+
+        const beforeText = element.textContent.substring(0, match.index);
+        const afterText = element.textContent.substring(match.index + match[0].length);
+
+        element.textContent = `${beforeText}$${value.toFixed(2)}${afterText}`;
+    }
+}
+
+const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+
+        mutation.addedNodes.forEach(monetiseEdrPoints);
+        monetiseEdrPoints(mutation.target);
+
+    }
+});
+
+const config = {
+    childList: true,
+    subtree: true,
+    characterData: true,
+    characterDataOldValue: true,
+};
+observer.observe(document.body, config);
